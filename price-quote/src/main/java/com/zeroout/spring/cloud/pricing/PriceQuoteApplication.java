3b06bbeeab160c21456496bd055e7b2e8fba533f
Alex <chengalex@outlook.com>
2020-04-06 01:11:12 +0800

    Fix appengine cfg

--- price-quote/src/main/java/com/zeroout/spring/cloud/pricing/PriceQuoteApplication.java
+++ price-quote/src/main/java/com/zeroout/spring/cloud/pricing/PriceQuoteApplication.java
@@ -2,11 +2,12 @@ package com.zeroout.spring.cloud.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-public class PriceQuoteApplication {
+public class PriceQuoteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PriceQuoteApplication.class, args);
